026607abe8addff4406da61ca44a404bb03b6175
Stanislav_Lushnikov <lust48h@gmail.com>
2023-11-15 20:39:03 +0300

    initial commit

---
+++ Task_2/Task_2/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.Calculator.calculatingDiscount;
+
+public class Main {
+    public static void main(String[] args) {
+        Test.checkCalculationDiscount();
+    }
+}
