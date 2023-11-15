bf51df0593849047018472eaa63ba0932a3607a2
Stanislav_Lushnikov <lust48h@gmail.com>
2023-11-15 20:41:23 +0300

    Revert "initial commit"
    This reverts commit 026607abe8addff4406da61ca44a404bb03b6175.

--- Task_2/Task_2/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.example.Calculator.calculatingDiscount;
-
-public class Main {
-    public static void main(String[] args) {
-        Test.checkCalculationDiscount();
-    }
-}
